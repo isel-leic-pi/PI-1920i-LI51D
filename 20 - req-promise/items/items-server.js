167d1018d911fd6e809b257205b19699e84178c8
Joao Trindade <jtrindade@cc.isel.ipl.pt>
2019-11-28 18:30:07 +0000

    Lecture 20

---
+++ 20 - req-promise/items/items-server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const DEFAULT_PORT = 8888
+
+const STORAGE_HOST = 'localhost'
+const STORAGE_PORT = 9200
+
+const http = require('http')
+const router = require('express')()
+
+const apiModule = require('./items-web-api')
+const serviceModule = require('./items-service');
+const storageModule = require('./items-storage');
+
+const storage = storageModule(STORAGE_HOST, STORAGE_PORT);
+const service = serviceModule(storage);
+const itemsApi = apiModule(router, service);
+
+http.createServer(router)
+    .listen(process.argv[2] || DEFAULT_PORT)
